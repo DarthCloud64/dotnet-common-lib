cf0657f6f148b001913489a5c8bec4c5d0b43e8b
Robert Reyes <neorob22@gmail.com>
2025-09-14 12:40:51 -0500

    update repo

--- Interfaces/IRepository.cs
+++ Interfaces/IRepository.cs
@@ -5,6 +5,6 @@ public interface IRepository<T> where T : class
     Task<T> Create(T entity, CancellationToken cancellationToken);
     Task<T> ReadById(Guid id, CancellationToken cancellationToken);
     Task<List<T>> Read(int offset, int maxResults, CancellationToken cancellationToken);
-    Task<T> Update(T entity);
-    Task Delete(T entity);
+    Task<T> Update(T entity, CancellationToken cancellationToken);
+    Task Delete(T entity, CancellationToken cancellationToken);
 }
